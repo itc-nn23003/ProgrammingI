c0d33b668737539a40c21070535507e5b25ad8e8
itc-nn23003 <n23003@std.it-college.ac.jp>
2023-07-12 06:37:00 +0000

    for文

---
+++ CHAPTER03/Q3_3_1.py
@@ -0,0 +1,17 @@
+my_list = [
+    "tokyo",
+    "osaka",
+    "fukuoka",
+    "aichi",
+    "kyoto",
+    "chiba",
+    "saitama",
+    "gunma",
+]
+my_list_6 = []
+for i in my_list:
+    if len(i) >= 6:
+        my_list_6.append(i)
+print(my_list_6)
+
+
