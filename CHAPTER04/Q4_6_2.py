3dfce4fcd7ac31641d87dbfee2e3b0b63f15602d
itc-nn23003 <n23003@std.it-college.ac.jp>
2023-07-26 07:10:14 +0000

    手直し

--- CHAPTER04/Q4_6_2.py
+++ CHAPTER04/Q4_6_2.py
@@ -8,13 +8,15 @@ def generate_students_data(num_students=10):
         height = random.randint(150, 190)
         weight = random.randint(50, 80)
         students_data.append((name, height, weight))
+
         if i == 0:
             print("i, name, height, weight")
         if i < 2 or i == num_students - 1:
             print(i, name, height, weight)
         elif i == 2:
             print("...")
-        return students_data
+
+    return students_data
 
 
 students_data = generate_students_data(10)
