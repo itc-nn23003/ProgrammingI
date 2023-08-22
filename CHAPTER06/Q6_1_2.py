6312adbcadac4fd3abf2493d95789cbbf8891e1c
itc-nn23003 <n23003@std.it-college.ac.jp>
2023-08-22 05:16:20 +0000

    クラス

--- CHAPTER06/Q6_1_2.py
+++ CHAPTER06/Q6_1_2.py
@@ -10,3 +10,10 @@ class Person:
         print("国籍:", self.nationality)
         print("生まれた年:", self.birth)
         print("住んでいる所:", self.address)
+
+
+heroine = Person("かぐや姫", "日本", "685", "静岡県富士市")
+print(heroine.show_attributes())
+
+hero = Person("金太郎", "日本", "956", "静岡県駿東郡小山町")
+print(hero.show_attributes())
